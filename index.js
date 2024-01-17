ae87867457bd062a59543cb8bba395d50e9c297d
abyola <abyola46@gmail.com>
2024-01-17 12:05:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+//let companyName = 'Scruber';
+
+const companyName = 'Scruber';__)))
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan smith';
+
